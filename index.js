b25688d31c5303966dd744a22343fdec7bda079a
BikashElangbam <58908215+BikashElangbam@users.noreply.github.com>
2022-12-22 13:22:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,61 @@
-// while typing validate the form and show message
-const email = document.querySelector("input[name='email']");
-const password = document.querySelector("input[name='password']");
-const repassword = document.querySelector("input[name='repassword']");
-const fullname = document.querySelector("input[name='fullname']");
-const form = document.getElementById('form');
-const emailregx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
-const nameregx = /^[a-zA-Z]+$/;
+const hour = document.querySelector('.hour');
+const minute = document.querySelector('.minute');
+const second = document.querySelector('.second');
+const currentDay = document.querySelector('.weekDay');
+const currentDate = document.querySelector('.dateToday');
+const dayPeriod = document.querySelector('.period');
+const timezones =['Asia/Calcutta', 'America/New_York', 'Europe/London'];
 
-form.addEventListener('submit', function(event){
-  
-  //Email Validation
-  if (email.value === '' || !emailregx.test(email.value)) {
-    email.parentElement.classList.add('form-field-error');
-    alert("Please enter a valid email address..");
-  } else {
-    email.parentElement.classList.remove('form-field-error');
-  }
-  
-  //Password Validation
-  if (password.value === '') {
-    password.parentElement.classList.add('form-field-error');
-    alert("password cannot be blank");
+function changeTimeZone(timezone){
+
     
-  } else {
-    password.parentElement.classList.remove('form-field-error');
-  }
 
-  //Re-Password Validation
-  if (repassword.value === '' || password.value != repassword.value) {
-        repassword.parentElement.classList.add('form-field-error');
-        alert("password does not match");
-        
-  } else {
-        repassword.parentElement.classList.remove('form-field-error');
-  }
-
-  // Name Validation
-   if(fullname.value === '' || !nameregx.test(fullname.value)){
-    fullname.parentElement.classList.add('form-field-error');
-    alert("Please enter a valid name..");
-   } else{
-    fullname.parentElement.classList.remove('form-field-error');
-   }
-
-  event.preventDefault();
+}
+function clock(){
+let today = new Date();
+let date = today.toLocaleString('en-US',{
+    timeZone: 'Asia/Calcutta',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    weekday:'long',
 });
+console.log(today);
+console.log(date);
+let day = date.slice(0,8);
+let hours = date.slice(22,24);
+let minutes = date.slice(25,27);
+let seconds = date.slice(28,30);
+let dateToday = date.slice(10,20);
+let period = date.slice(31);
+
+hour.innerHTML = hours;
+minute.innerHTML = minutes;
+second.innerHTML = seconds;
+currentDay.innerHTML = day;
+currentDate.innerHTML = dateToday;
+dayPeriod.innerHTML = period;
+}
+
+const stop = setInterval(clock, 1000);
+function pause(){
+        clearInterval(stop);
+}
+
+function play(){
+    setInterval(clock, 1000);
+}
+
+
+
+
+
+
+// {timeZone: 'Asia/Calcutta'}
+// {timeZone: 'America/New_York'}
 
 
 
