f69c72e96f243e8db4a7b137abe404fb5966e7e4
BikashElangbam <58908215+BikashElangbam@users.noreply.github.com>
2022-12-28 11:16:50 +0530

    Add files via upload

--- DragDrop/index.js
+++ DragDrop/index.js
@@ -1,1 +1,27 @@
+function sourcedrag(event) {
+  event.dataTransfer.setData('sourceDataId', event.target.id);
+}
+function sourcedrop(event) {
+  const id = event.dataTransfer.getData('sourceDataId');
+  const el = document.getElementById(id);
+  event.target.appendChild(el);
+    
+}
+  
+function allowDrop(event) {
+  event.preventDefault();
+}
+  
+function destinationdrag(event) {
+  event.dataTransfer.setData('destinationDataId', event.target.id);
+}
 
+function destinationdrop(event) {
+  const id = event.dataTransfer.getData('destinationDataId');
+  const el = document.getElementById(id);
+  event.target.appendChild(el);
+    
+}
+  
+  
+  
